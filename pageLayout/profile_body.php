7569b3f7334a25802def92c68730303ef0bbbe8c
RuiCS <rui.pedro.csoares@gmail.com>
2016-12-12 18:10:31 +0000

    CSS - Changed Profile to show Restaurants and Reviews

--- pageLayout/profile_body.php
+++ pageLayout/profile_body.php
@@ -23,7 +23,6 @@
             }else{
         ?>
         <div id = "details_area">
-            <br>
             <div id="username_and_edit">
                 <div id="profile_username">
                     <?php echo '@'.$user['username'];?>
@@ -37,73 +36,79 @@
                     </div>
                 <?php } ?>  
             </div>
-            
-            <div id="account_details">
-                <div id="account_header">
-                    Account Details:
+            <div id= "account" />
+                <div id="profile_image">
+                        aqui vai a imagem
                 </div>
-                <div id="profile_type">
-                    <?php echo $user['type'];?>
-                </div>
-                <div id="profile_name">
-                    <?php 
-                        if($user['name'] != null)
-                             echo $user['name'];
-                        else echo 'Unkown birthday';
-                    ?>
-                </div>
-                <div id="profile_email">
-                    <?php echo $user['email'];?>
-                </div>
+                <div id="account_details">
+                    <div id="account_header">
+                        Account Details:
+                    </div>
+                    <div id="profile_type">
+                        <?php echo $user['type'];?>
+                    </div>
+                    <div id="profile_name">
+                        <?php 
+                            if($user['name'] != null)
+                                 echo $user['name'];
+                            else echo 'Unkown birthday';
+                        ?>
+                    </div>
+                    <div id="profile_email">
+                        <?php echo $user['email'];?>
+                    </div>
 
-                <div id="profile_birthdate">
-                    <?php 
-                        if($user['birth'] != null)
-                             echo $user['birth'];
-                        else echo 'Unkown birthday';
-                    ?>
+                    <div id="profile_birthdate">
+                        <?php 
+                            if($user['birth'] != null)
+                                 echo $user['birth'];
+                            else echo 'Unkown birthday';
+                        ?>
+                    </div>
+                    <div id="profile_place">
+                        <?php 
+                            if($user['city'] != null)
+                                 echo $user['city'];
+                            if($user['city'] != null && $user['country'] != null)
+                                echo ', ';
+                            if($user['country'] != null)
+                                echo $user['country'];
+                        ?>
+                    </div>
                 </div>
-                <div id="profile_place">
+                <div id="profile_description">
                     <?php 
-                        if($user['city'] != null)
-                             echo $user['city'];
-                        if($user['city'] != null && $user['country'] != null)
-                            echo ', ';
-                        if($user['country'] != null)
-                            echo $user['country'];
+                        if($user['description'] != null)
+                             echo $user['description'];
+                        else echo 'This user has no description.';
                     ?>
                 </div>
             </div>
-            <div id="profile_description">
-                <?php 
-                    if($user['description'] != null)
-                         echo $user['description'];
-                    else echo 'This user has no description.';
+            <div id="owned_or_reviews">
+                <?php
+                    if($user['type'] == 'owner'){
+                        echo '<label id="profile_list_header">OWNED RESTAURANTS</label>';
+                        $owned_restaurants = getRestaurantsByUser($dbh,$_GET['username']);
+                        foreach($owned_restaurants as $owned_restaurant){
+                            echo '<div class="searched_res">';
+                            echo '<label class="name">' . $owned_restaurant['name'] . '</label>';
+                            echo '<br>';
+                            echo '<label class="rating">' . $owned_restaurant['average_score'] . '</label>';
+                            echo '<br>';
+                            echo '<label class="address">' . $owned_restaurant['address'] . ' '. $owned_restaurant['city'] . ' ' . $owned_restaurant['country'] . '</label>';
+                            echo '</div>';
+                        }
+                    }else{
+                        echo '<label id="profile_list_header">POSTED REVIEWS</label>';
+                        $posted_reviews = getReviewsByUser($dbh,$_GET['username']);
+                        foreach($posted_reviews as $posted_review){
+                            echo '<div class="searched_res">';
+
+                            echo '</div>';
+                        }
+                    }
                 ?>
             </div>
-            <div id="profile_image">
-                    aqui vai a imagem
-            </div>
-
-            <?php
-                if($user['type'] == 'owner'){
-                    $owned_restaurants = getRestaurantsByUser($dbh,$_GET['username']);
-                    foreach($owned_restaurants as $owned_restaurant){
-                        echo '<div class="searched_res"';
-                        echo '<label class="name">' . $owned_restaurant['name'] . '</label>';
-                        echo '<label class="rating">' . $owned_restaurant['average_score'] . '</label>';
-                        echo '<label class="address">' . $owned_restaurant['address'] . ' '. $owned_restaurant['city'] . ' ' . $owned_restaurant['country'] . '</label>';
-                        echo '</div>';
-                    }
-                }else{
-                    $posted_reviews = getReviewsByUser($dbh,$_GET['username']);
-                    foreach($posted_reviews as $posted_review){
-                        echo '<div class="searched_res"';
-
-                        echo '</div>';
-                    }
-                }
-            ?>
             
         </div>
         <?php } ?>
